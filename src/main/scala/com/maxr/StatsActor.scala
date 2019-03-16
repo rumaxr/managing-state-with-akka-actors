24dbc3eef6fe890b47b299d4ca0e3591a24622c3
Max R <max.remkes@gmail.com>
2019-03-16 13:12:52 +0100

    Use become to get rid of var

--- src/main/scala/com/maxr/StatsActor.scala
+++ src/main/scala/com/maxr/StatsActor.scala
@@ -1,31 +1,45 @@
 package com.maxr
 
-import akka.actor.{Actor, ActorLogging}
+import akka.actor.{Actor, ActorLogging, Props}
 
 case object GetStats
 case class  StatsResult(stats:Map[String,Int])
 
-class StatsActor extends Actor with ActorLogging {
-  private def wordsToMapCount(line: String): Map[String, Int] = {
+object StatsActor {
+  case class StatsMaps(wordCountMap: Map[String, Int] = Map.empty.withDefaultValue(0))
+
+  def props(): Props = Props[StatsActor]
+
+  def wordsToMapCount(line: String): Map[String, Int] = {
     line
       .split("\\W+")
       .foldLeft(Map.empty[String, Int])((map: Map[String, Int], next: String) => map + (next -> (map.getOrElse(next, 0) + 1)))
   }
+}
 
-  var statsMap: Map[String, Int] = Map.empty.withDefaultValue(0)
+class StatsActor extends Actor with ActorLogging {
+
+  import StatsActor._
 
   override def receive: Receive = {
+    updated(StatsMaps())
+  }
+
+  private def updated(statsMap: StatsMaps): Receive = {
     case Message(line: String) =>
 
       log.info(s"New message: $line")
 
       val currentMap: Map[String, Int] = wordsToMapCount(line)
-
-      statsMap ++= currentMap.map {
-        case (k, v) => k -> (v + statsMap(k))
+      val wordCountMap: Map[String, Int] = statsMap.wordCountMap ++ currentMap.map {
+        case (k, v) => k -> (v + statsMap.wordCountMap(k))
       }
+      context.become(updated(statsMap.copy(wordCountMap = wordCountMap)))
 
     case GetStats =>
-      sender ! StatsResult(statsMap)
+      sender ! StatsResult(statsMap.wordCountMap)
+
+    case _ => throw new IllegalStateException("Unknown event was sent to StatsActor")
+
   }
 }
